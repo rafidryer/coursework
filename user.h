cd8efa70e5a817aed8758e8a3f92905c3cd65929
rafidryer <78875183+rafidryer@users.noreply.github.com>
2022-04-13 12:31:30 +0100

    d

--- user.h
+++ user.h
@@ -23,8 +23,8 @@ int getpid(void);
 char* sbrk(int);
 int sleep(int);
 int uptime(void);
-int mprotect(void *addr, int len);
-int munprotect(void *addr, int len);
+int mprotect(void*, int );
+int munprotect(void*, int );
 
 // ulib.c
 int stat(const char*, struct stat*);
