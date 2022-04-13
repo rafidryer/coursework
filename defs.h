5d543f1d20326bc387ca5d251720627d4eadaf4b
rafidryer <78875183+rafidryer@users.noreply.github.com>
2022-04-13 10:47:36 +0100

    remove mprotect file calls

--- defs.h
+++ defs.h
@@ -185,8 +185,6 @@ void            switchuvm(struct proc*);
 void            switchkvm(void);
 int             copyout(pde_t*, uint, void*, uint);
 void            clearpteu(pde_t *pgdir, char *uva);
-int             mprotect(void *addr, int len);
-int             munprotect(void *addr, int len);
 
 
 // number of elements in fixed-size array
