e8594e0ded3e72170f538aacaf3bbbf4371084f7
rafidryer <78875183+rafidryer@users.noreply.github.com>
2022-04-13 10:09:40 +0100

    move mprotect to file

--- mprotect.c
+++ mprotect.c
@@ -3,6 +3,7 @@
 #include "user.h"
 #include "mmu.h"
 #include "fcntl.h"
+#include "proc.h"
 
  
 int 
@@ -10,15 +11,49 @@ main(int argc,char *argv[]) {
 
     printf(1, "mProtect file\n\n");
 
-    // struct proc *current = myproc();
-    int *str;
-    str = (int*)PGROUNDUP((uint)sbrk(0));
-    int len = 1;
+    struct proc *currentproc = myproc();
+    // int *str;
+    // str = (int*)PGROUNDUP((uint)sbrk(0));
+    int len;
+    int addr;
+    argint(0, &addr);
+    argint(1,&len);
 
 
-    printf(1, "return val of system call is %d\n");
-    printf(1, "Congrats !! You have successfully added new system  call in xv6 OS :) \n");
-    printf(1,"\n\nrunning MPROTECT\n");
-    mprotect((void *)str, len);
+    // printf(1, "return val of system call is %d\n");
+    // printf(1, "Congrats !! You have successfully added new system  call in xv6 OS :) \n");
+    // printf(1,"\n\nrunning MPROTECT\n");
+    // mprotect((void *)str, len);
+    if (len <= 0){
+        printf(1,"\nMprotect Failed");
+        return -1;
+    }
+
+    // check the address is greater then zero
+    if ((int)(&addr) % PGSIZE != 0){
+        return -1;
+    }
+
+    // int current = (int) addr;
+    pte_t *pageTableEntry;
+
+    // iterate through the pages in the range
+    int current = (int) addr;
+    for (int i = 0; i < len; ++i) {
+        pageTableEntry = walkpgdir(currentproc->pgdir, (void *) current, 0);
+        if (pageTableEntry == 0){
+            printf(1,"\nMprotect Failed");
+            return -1;
+        }
+        
+        // set the page to be writable
+        *pageTableEntry |= PTE_W;
+        current += PGSIZE;
+    }
+
+    lcr3(V2P(currentproc->pgdir));
+    printf(1,"\nMprotect Passed");
+
+    return 0;
     exit();
  }
